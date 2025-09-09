bedc6eda3a8a29df24cfc4c027cc26449b1ad971
Chandupa Ranawaka <116500003+Chandupa2001@users.noreply.github.com>
2025-09-09 21:47:27 +0530

    Fixed issue in order receipt

--- src/Orders/OrderDetailsModal.jsx
+++ src/Orders/OrderDetailsModal.jsx
@@ -342,7 +342,7 @@ const OrderDetailsModal = ({ open, onClose, order }) => {
           </Box>
           <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
             <Typography sx={{ fontWeight: 'bold', color: '#000' }}>Payment:</Typography>
-            <Typography sx={{ color: '#000' }}>{order.payment}</Typography>
+            <Typography sx={{ color: '#000' }}>{order.paymentMethod}</Typography>
           </Box>
           {/* <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
             <Typography sx={{ fontWeight: 'bold', color: '#000' }}>Status:</Typography>
